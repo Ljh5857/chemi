be76fcefc2b7a8c42ff5b93f023873d5e75c6e0a
DESKTOP-BG79OAA\user1 <temp>
2024-06-24 09:35:32 +0900

    Chemi 기본set

---
+++ src/main/java/com/chemi/ChemiApplication.java
@@ -0,0 +1,13 @@
+package com.chemi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChemiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChemiApplication.class, args);
+	}
+
+}
