e9cf919a1e6474f7d1b0433b4ba18f0ca20879b7
DESKTOP-BG79OAA\user1 <temp>
2024-06-24 10:21:04 +0900

    intelliJ check2

---
+++ src/test/java/com/chemi/ChemiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chemi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChemiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
