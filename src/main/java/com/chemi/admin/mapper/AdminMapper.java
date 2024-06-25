c8d914b970a7d627562563cecc57e7eee21ab57c
DESKTOP-M9NJ13P\user1 <hongjy3458@gmail.com>
2024-06-25 14:20:40 +0900

    pwd 확인 추가

--- src/main/java/com/chemi/admin/mapper/AdminMapper.java
+++ src/main/java/com/chemi/admin/mapper/AdminMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface AdminMapper {
 
-    @Select("SELECT * FROM ADMIN WHERE ID = #{id}")
+    @Select("SELECT * FROM ADMIN WHERE ID = #{id} AND PWD = #{pwd}")
     AdminVo login(AdminVo adminVo);
 
 
