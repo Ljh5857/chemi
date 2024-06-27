dda903fb47f19c4b9309b74453e83588b8e92396
DESKTOP-M9NJ13P\user1 <hongjy3458@gmail.com>
2024-06-27 17:48:22 +0900

    6/27 수정사항 ADMIN기준 물품등록+수정

---
+++ src/main/java/com/chemi/admin/vo/PrdImgVo.java
@@ -0,0 +1,11 @@
+package com.chemi.admin.vo;
+
+import lombok.Data;
+
+@Data
+public class PrdImgVo {
+    private String no;
+    private String prdNo;
+    private String delYn;
+    private String photo;
+}
